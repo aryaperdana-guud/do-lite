b7625719454443bfd7fbfc04241f71a166fa1cf7
aryaperdana-guud <arya.perdana@guud.company>
2025-02-07 15:45:14 +0700

    springboot 2.6.14

---
+++ springboot/src/main/java/com/gli/clic/ClicApplication.java
@@ -0,0 +1,13 @@
+package com.gli.clic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClicApplication.class, args);
+	}
+
+}
---
+++ springboot/src/test/java/com/gli/clic/ClicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gli.clic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
