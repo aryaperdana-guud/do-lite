23cbdf490cb7b0975d2cb1804d6007199cb6096b
aryaperdana-guud <arya.perdana@guud.company>
2025-02-12 10:08:04 +0700

    [BE] modify login response into basic object

--- springboot/src/main/java/com/gli/clic/auth/controllers/AuthController.java
+++ springboot/src/main/java/com/gli/clic/auth/controllers/AuthController.java
@@ -3,6 +3,7 @@ package com.gli.clic.auth.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.springframework.http.HttpStatus;
@@ -16,14 +17,16 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class AuthController {
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Map<String, String> credentials) {
+    public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {
         String userId = credentials.get("userid");
         String password = credentials.get("password");
 
         if ("USER".equals(userId) && "PASS".equals(password)) {
-            return ResponseEntity.ok("Login successful");
+            Map<String, String> response = new HashMap<>();
+            response.put("token", "jtwgeneratedtoken");
+            return ResponseEntity.ok(response);
         } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
         }
     }
     
