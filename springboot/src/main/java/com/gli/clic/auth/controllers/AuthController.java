206222f62ff81f000e29fecffb763eb2ebcc3326
aryaperdana-guud <arya.perdana@guud.company>
2025-02-12 10:22:43 +0700

    [BE] modify login, implement jwt token

--- springboot/src/main/java/com/gli/clic/auth/controllers/AuthController.java
+++ springboot/src/main/java/com/gli/clic/auth/controllers/AuthController.java
@@ -3,6 +3,10 @@ package com.gli.clic.auth.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,14 +20,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 @RequestMapping("/auth")
 public class AuthController {
 
+    String jwtsecretkey = "5882985c8b1e2dce2763072d56a1d6e5";
+
     @PostMapping("/login")
     public ResponseEntity<Map<String, String>> login(@RequestBody Map<String, String> credentials) {
         String userId = credentials.get("userid");
         String password = credentials.get("password");
 
         if ("USER".equals(userId) && "PASS".equals(password)) {
+            String token = Jwts.builder()
+                    .setSubject(userId)
+                    .setIssuedAt(new Date())
+                    .setExpiration(new Date(System.currentTimeMillis() + 86400000)) // 1 day expiry
+                    .signWith(SignatureAlgorithm.HS256, jwtsecretkey)
+                    .compact();
+
             Map<String, String> response = new HashMap<>();
-            response.put("token", "jtwgeneratedtoken");
+            response.put("token", token);
             return ResponseEntity.ok(response);
         } else {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(null);
