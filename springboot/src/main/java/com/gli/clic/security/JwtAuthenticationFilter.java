1bbc88d8e7116753874ee76d0708499f28562c7e
drydry <audry.a.wijaya@gmail.com>
2025-02-18 11:15:03 +0700

    [BE]

--- springboot/src/main/java/com/gli/clic/security/JwtAuthenticationFilter.java
+++ springboot/src/main/java/com/gli/clic/security/JwtAuthenticationFilter.java
@@ -8,8 +8,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.Collections;
--- springboot/src/main/java/com/gli/clic/security/SecurityConfig.java
+++ springboot/src/main/java/com/gli/clic/security/SecurityConfig.java
@@ -3,16 +3,13 @@ package com.gli.clic.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.authentication.AuthenticationManager;
 
 @Configuration
 @EnableWebSecurity
